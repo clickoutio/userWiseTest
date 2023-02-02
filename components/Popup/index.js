a5bf4ac36300e52120af6ed807adb6ca4dff58a0
Javier Morales de Vera <javierMorales9@gmail.com>
2023-02-02 18:04:49 +0100

    seting up the good templates

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -18,50 +18,52 @@ const Popup = React.forwardRef(({handleUserClickOnATemplate}, ref) => {
                 <span className={"text-sm text-gray-400"}>Create flow from Scratch</span>
               </div>
             </div>
-            <FlowTemplates title={"Prevent Delinquent Churn"} tags={["Churn Prevention"]}
+            <FlowTemplates title={"Capture Leads From a Typeform, and Segment Based on Value"}
+                           tags={["Nurture", "Leads", "Onboarding"]}
                            onClick={handleUserClickOnATemplate}>
+              Capture lead information in a form and segment leads based on their Budget input. Send low-value leads an
+              automated email sequence. Assign high-value leads to a sales rep and ask them to book a demo.
+            </FlowTemplates>
+            <FlowTemplates title={"Get Feedback From a Recently Churned Customer"} tags={["Customer Success"]}
+                           onClick={handleUserClickOnATemplate}>
+              Ask recently churned customers for feedback to improve product and customer experience. Most people go
+              silent once they churn, so we're going to follow up!
+            </FlowTemplates>
+            <FlowTemplates title={"Follow Up On Pricing Page Visit"}
+                           tags={["Leads"]} onClick={handleUserClickOnATemplate}>
+              If a user visits your pricing page, send them an email.
+            </FlowTemplates>
+            <FlowTemplates title={"Educate Users About a Recently Activated Feature"}
+                           tags={["Product Adoption"]} onClick={handleUserClickOnATemplate}>
+              If a user starts using or activates a specific feature, send them an educational email about that feature
+              to increase feature adoption.
+            </FlowTemplates>
+            <FlowTemplates title={"Engage Your Important Trials on Facebook"} tags={["Trials"]}
+                           onClick={handleUserClickOnATemplate}>
+              Add users that have signed up for a trial for a more expensive plan to a Facebook audience and engage them
+              with Facebook Ads with case studies.
+            </FlowTemplates>
+            <FlowTemplates title={"Nudge Long-Term Customer to Upgrade to Annual Subscription"}
+                           tags={["Onboarding", "Trials"]} onClick={handleUserClickOnATemplate}>
+              If a user has completed the first step of your sign-up flow but hasn’t finished the rest of the steps (for
+              example, not provided billing details), send them a helpful reminder email.
+            </FlowTemplates>
+            <FlowTemplates title={"Prevent Delinquent Churn"}
+                           tags={["Churn Prevention"]} onClick={handleUserClickOnATemplate}>
               Follow up on failed payments to prevent churn. Add tag PaymentFailed when a subscription is Past Due and
               remove it when subscription becomes Active again.
             </FlowTemplates>
-            <FlowTemplates title={"Prevent Inactive Users from Churning"} tags={["Churn Prevention"]}
-                           onClick={handleUserClickOnATemplate}>
-              If a customer is not active for more than 10 days, send “Do you need help?” email. If they don't open
-              the
+            <FlowTemplates title={"Prevent Inactive Customers From Churning"}
+                           tags={["Churn Prevention"]} onClick={handleUserClickOnATemplate}>
+              If a customer is not active for more than 10 days, send “Do you need help?” email. If they don't open the
               email start showing them successful case studies via Facebook ads to reactivate them. Stop showing them
               Facebook Ads when they become active again.
             </FlowTemplates>
-            <FlowTemplates title={"Capture Leads With a Lead Magnet and Send a Nurture Sequence"}
-                           tags={["Nurture", "Leads"]} onClick={handleUserClickOnATemplate}>
-              When a person downloads an eBook, cheatsheet or another lead magnet through a form on your site: 1. Tag
-              them
-              2. Send them a nurturing email sequence.
-            </FlowTemplates>
             <FlowTemplates title={"Convert Trial Users With Targeted Onboarding Campaigns"}
-                           tags={["Onboarding", "Trials"]} onClick={handleUserClickOnATemplate}>
+                           tags={["Onboarding", "Trials"]}
+                           onClick={handleUserClickOnATemplate}>
               Send Trial users the most relevant content for them with the help of user data that you’ve collected
-              through
-              the sign-up process.
-            </FlowTemplates>
-            <FlowTemplates title={"Double the Open Rate of a Newsletter Broadcast"} tags={["Email Broadcast"]}
-                           onClick={handleUserClickOnATemplate}>
-              Send a follow-up email with a different subject line to people who haven’t opened your first broadcast.
-              That
-              way you’re going to drastically increase the open rate of the broadcast with almost no extra efforts.
-            </FlowTemplates>
-            <FlowTemplates title={"Capture Mailchimp Subscribers With a Lead Magnet and Send a Nurture Sequence"}
-                           tags={["Nurture", "Leads"]} onClick={handleUserClickOnATemplate}>
-              When a MailChimp subscriber downloads an eBook through a specific form on your site: 1. Tag them 2. Send
-              them a nurturing email sequence.
-            </FlowTemplates>
-            <FlowTemplates title={"Remind a Person to Download a Lead Magnet"} tags={["Nurture", "Leads"]}
-                           onClick={handleUserClickOnATemplate}>
-              When a person leaves their email on a lead magnet on your website but doesn’t click on the Download link
-              in
-              the confirmation email, remind them with a follow-up email.
-            </FlowTemplates>
-            <FlowTemplates title={"Onboard Trial Users and Help Them Reach the 'Aha' Moment"}
-                           tags={["Onboarding", "Trials"]} onClick={handleUserClickOnATemplate}>
-              When a user signs up for your product, send them an onboarding sequence.
+              through the sign-up process.
             </FlowTemplates>
           </div>
         </div>
