0127582cf2c1314f08a9effe38d19db1455e0b39
Javier Morales de Vera <javierMorales9@gmail.com>
2023-03-08 10:24:11 +0100

    delete authentication

--- components/Navigation/TopBar.js
+++ components/Navigation/TopBar.js
@@ -4,16 +4,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import routes from '../../utils/routes';
 import logo from '../../public/images/logo-primary.svg';
-import UserIcon from '../../public/images/icons/account_circle.svg';
-import Dropdown from '../Dropdown';
 import styles from './TopBar.module.css';
-import {signOut, useSession} from "next-auth/react";
 
 const links = [];
 
 const TopBar = ({active}) => {
-  const {session: accountData} = useSession();
-
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
@@ -22,9 +17,6 @@ const TopBar = ({active}) => {
 
             <Image
               src={logo}
-              layout={'fill'}
-              objectFit={'contain'}
-              objectPosition={'center'}
             />
 
           </Link>
@@ -47,31 +39,6 @@ const TopBar = ({active}) => {
           ))}
         </div>
         <div className={styles.settings}>
-          <Dropdown>
-            <Dropdown.Button className={styles.button} variant={'outline'}>
-              {accountData?.imageUrl ? (
-                <img
-                  className={styles.companyLogo}
-                  src={accountData?.imageUrl}
-                />
-              ) : (
-                <Image
-                  src={UserIcon}
-                  layout={'fill'}
-                  objectFit={'contain'}
-                  objectPosition={'center'}
-                />
-              )}
-              {/*<Image src={UserIcon} height={32} width={32} />*/}
-            </Dropdown.Button>
-            <Dropdown.Menu>
-              <div role={'none'}>
-                <Dropdown.Link href={"/"} onClick={signOut}>
-                  Sign out
-                </Dropdown.Link>
-              </div>
-            </Dropdown.Menu>
-          </Dropdown>
         </div>
       </div>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect} from 'react';
 import TopBar from '../components/Navigation/TopBar';
 import DotIcon from '../public/images/icons/dot.inline.svg';
 import addIcon from '../public/images/icons/add.svg';
@@ -6,13 +6,9 @@ import Button from '../components/Button';
 import styles from './index.module.css';
 import Popup from "../components/Popup";
 import CongratsPopup from "../components/Popup/congrats";
-import {useSession} from "next-auth/react";
 import usePopupVisible from "../hocs/usePopupVisible";
-import Router from "next/router";
 
 function Flows() {
-  console.log(useSession());
-
   const [ref, isPopupVisible, setIsPopupVisible] =
     usePopupVisible(false);
 
@@ -30,13 +26,6 @@ function Flows() {
     window.addEventListener('keydown', close)
     return () => window.removeEventListener('keydown', close)
   }, []);
-
-  const {session: userData, status} = useSession();
-
-  if (status === 'loading') return <div>Loading...</div>
-
-  if (status !== "authenticated")
-    return Router.push("/api/auth/signin")
 
   const handleUserClickOnATemplate = () => {
     console.log('handleUserClickOnATemplate');
@@ -89,7 +78,7 @@ function Flows() {
       {isPopupVisible && <Popup ref={ref} handleUserClickOnATemplate={handleUserClickOnATemplate}/>}
       <TopBar active={'flows'}/>
       <div className={styles.info}>
-        <h2 className={styles.userName}>👋🏼 Hello {userData?.firstName}</h2>
+        <h2 className={styles.userName}>👋🏼 Hello</h2>
         <p>Create your first flow now and start saving clients</p>
         <div className={styles.stats}>
           {stats.map(({id, iconClass, count, title}) => (
