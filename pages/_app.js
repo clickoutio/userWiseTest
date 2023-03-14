d5a728f176163a3acfe1598123f2b7c0a4c447bb
Javier Morales de Vera <javierMorales9@gmail.com>
2023-03-14 11:09:24 +0100

    trying a new thing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
-import dynamic from 'next/dynamic';
 import '../styles/global.css';
+import _refiner from "refiner-js";
 
 export default function App({Component, pageProps: {session, ...pageProps}}) {
   useEffect(() => {
@@ -13,15 +13,14 @@ export default function App({Component, pageProps: {session, ...pageProps}}) {
     window['JIMO_PROJECT_ID'] = '48512554-0bcc-4e06-aad3-f65867aa11ed'; // Update this
 
     document.getElementsByTagName('head')[0].appendChild(s);
-  }, []);
 
-  const _refiner = dynamic(() => import('refiner-js'), {ssr: false});
-  _refiner('setProject', '431bd1e0-bd0f-11ed-8bb0-a7f70ccf4803');
-  _refiner('identifyUser', {
-    id: 'USER-ID-ABC-123', // Replace with your user ID
-    email: 'jane@awesome.com', // Replace with user Email
-    name: 'Jane Doe', // Replace with user name
-  });
+    _refiner('setProject', '431bd1e0-bd0f-11ed-8bb0-a7f70ccf4803');
+    _refiner('identifyUser', {
+      id: 'USER-ID-ABC-123', // Replace with your user ID
+      email: 'jane@awesome.com', // Replace with user Email
+      name: 'Jane Doe', // Replace with user name
+    });
+  }, []);
 
   return (
     <Component {...pageProps} />
